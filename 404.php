5b2c7a83d0aaba97eaf0d223bdcb40817bee008c
Radosław Kuświk <radoslaw.kuswik@gmail.com>
2019-12-01 20:34:02 +0100

    [#14] Page 404 animation

--- 404.php
+++ 404.php
@@ -23,6 +23,35 @@
   ];
   $ContentPage = '
     <p>Strona nie została odnaleziona!</p>
+    <div
+      class="post-error-404-animation__wrapper"
+      aria-label="Animacja przedstawia rozdymkę unoszącą się na wodzie"
+    >
+      <img
+        alt=""
+        aria-hidden="true"
+        src="'.get_template_directory_uri().'/images/post-error-404-animation-fish.png"
+        class="post-error-404-animation__fish"
+      />
+      <img
+        alt=""
+        aria-hidden="true"
+        src="'.get_template_directory_uri().'/images/post-error-404-animation-wave.png"
+        class="post-error-404-animation__wave post-error-404-animation__wave-1"
+      />
+      <img
+        alt=""
+        aria-hidden="true"
+        src="'.get_template_directory_uri().'/images/post-error-404-animation-wave.png"
+        class="post-error-404-animation__wave post-error-404-animation__wave-2"
+      />
+      <img
+        alt=""
+        aria-hidden="true"
+        src="'.get_template_directory_uri().'/images/post-error-404-animation-wave.png"
+        class="post-error-404-animation__wave post-error-404-animation__wave-3"
+      />
+    </div>
     <p>Skorzystaj z podpowiedzi wpisów poniżej lub przejdź na <a href="'.home_url().'">Stronę Główną</a></p>
   ';
 ?>
