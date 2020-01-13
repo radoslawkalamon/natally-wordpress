b9d2a6ae886f9b76cc73c16bb7832f171aaef2b0
Radosław Kalamon <16870210+radoslawkalamon@users.noreply.github.com>
2020-01-13 15:26:17 +0100

    [#47] Favicons changes

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
 <?php get_template_part('components/the-header'); ?>
+<?php $urlFavicon = get_template_directory_uri()."/images/icons/"; ?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
@@ -12,33 +14,17 @@
     <meta charset="<?php bloginfo('charset'); ?>" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <?php $urlFavicon = get_template_directory_uri()."/images/icons/"; ?>
-    <!-- MS Tile Settings -->
-    <meta name="msapplication-TileColor" content="#FFFFFF" />
-    <meta name="msapplication-square70x70logo" content="<?= $urlFavicon; ?>icon-192x192.png">
-    <meta name="msapplication-square150x150logo" content="<?= $urlFavicon; ?>icon-192x192.png">
-    <meta name="msapplication-wide310x150logo" content="<?= $urlFavicon; ?>icon-310x150.png">
+    <meta name="format-detection" content="telephone=no">
     <!-- Favicon Settings -->
-    <link rel="icon" type="image/png" href="<?= $urlFavicon; ?>icon-192x192.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="<?= $urlFavicon; ?>icon-192x192.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="<?= $urlFavicon; ?>icon-192x192.png" sizes="48x48" />
-    <link rel="icon" type="image/png" href="<?= $urlFavicon; ?>icon-192x192.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="<?= $urlFavicon; ?>icon-192x192.png" sizes="144x144" />
-    <link rel="icon" type="image/png" href="<?= $urlFavicon; ?>icon-192x192.png" sizes="192x192" />
+    <link rel="shortcut icon" type="image/png" href="<?= $urlFavicon; ?>icon-192x192.png">
+    <link rel="shortcut icon" sizes="192x192" href="<?= $urlFavicon; ?>icon-192x192.png">
     <!-- iOS Settings -->
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="format-detection" content="telephone=no">
-    <link href="<?= $urlFavicon; ?>icon-192x192.png" sizes="152x152" rel="apple-touch-icon-precomposed">
-    <link href="<?= $urlFavicon; ?>icon-192x192.png" sizes="144x144" rel="apple-touch-icon-precomposed">
-    <link href="<?= $urlFavicon; ?>icon-192x192.png" sizes="120x120" rel="apple-touch-icon-precomposed">
-    <link href="<?= $urlFavicon; ?>icon-192x192.png" sizes="114x114" rel="apple-touch-icon-precomposed">
-    <link href="<?= $urlFavicon; ?>icon-192x192.png" sizes="76x76" rel="apple-touch-icon-precomposed">
-    <link href="<?= $urlFavicon; ?>icon-192x192.png" sizes="72x72" rel="apple-touch-icon-precomposed">
-    <link href="<?= $urlFavicon; ?>icon-192x192.png" sizes="57x57" rel="apple-touch-icon-precomposed">
+    <link rel="apple-touch-icon" href="<?= $urlFavicon; ?>icon-192x192-white.png">
     <!-- Blink Settings -->
     <meta name="theme-color" content="#E2000E">
     <!-- Google Podcasts Settings -->
-    <link type="application/rss+xml" rel="alternate" title="169cm.pl :: Kanał Podcastu" href="http://feeds.soundcloud.com/users/soundcloud:users:618891150/sounds.rss"/>
+    <link type="application/rss+xml" rel="alternate" title="169cm.pl :: Kanał podcastu" href="http://feeds.soundcloud.com/users/soundcloud:users:618891150/sounds.rss"/>
     <!-- Posts RSS Feed -->
     <link type="application/rss+xml" rel="alternate" title="169cm.pl :: Kanał postów" href="<?= bloginfo('rss2_url'); ?>" />
     <?php wp_head(); ?>
