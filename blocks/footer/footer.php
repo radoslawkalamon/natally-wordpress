bfddcc535525f90a9cf3f2b1d9a0fe56b12ab9ce
Radoslaw Kalamon <16870210+radoslawkalamon@users.noreply.github.com>
2020-10-10 23:03:59 +0200

    [2.1.0] Footer section remove

--- blocks/footer/footer.php
+++ blocks/footer/footer.php
@@ -1,38 +1,34 @@
 <?php function Block_Footer() { ?>
-  <?php $socialMedia = [
-    [
-      'alt' => 'Odwiedź mnie na Facebooku',
-      'classname' => 'facebook',
-      'icon' => 'facebook.svg',
-      'link' => 'https://www.facebook.com/169cmpl',
-    ],
-    [
-      'alt' => 'Odwiedź mnie na Instagramie',
-      'classname' => 'instagram',
-      'icon' => 'instagram.svg',
-      'link' => 'https://instagram.com/radoslawkalamon',
-    ],
-    [
-      'alt' => 'Odwiedź mnie na Twitterze',
-      'classname' => 'twitter',
-      'icon' => 'twitter.svg',
-      'link' => 'https://twitter.com/radoslawkalamon',
-    ],
-    [
-      'alt' => 'Odwiedź mnie na SoundCloud',
-      'classname' => 'soundcloud',
-      'icon' => 'soundcloud.svg',
-      'link' => 'https://soundcloud.com/169cmpl',
-    ],
-  ]; ?>
+  <?php
+  // $socialMedia = [
+  //   [
+  //     'alt' => 'Odwiedź mnie na Facebooku',
+  //     'classname' => 'facebook',
+  //     'icon' => 'facebook.svg',
+  //     'link' => 'https://www.facebook.com/169cmpl',
+  //   ],
+  //   [
+  //     'alt' => 'Odwiedź mnie na Instagramie',
+  //     'classname' => 'instagram',
+  //     'icon' => 'instagram.svg',
+  //     'link' => 'https://instagram.com/radoslawkalamon',
+  //   ],
+  //   [
+  //     'alt' => 'Odwiedź mnie na Twitterze',
+  //     'classname' => 'twitter',
+  //     'icon' => 'twitter.svg',
+  //     'link' => 'https://twitter.com/radoslawkalamon',
+  //   ],
+  //   [
+  //     'alt' => 'Odwiedź mnie na SoundCloud',
+  //     'classname' => 'soundcloud',
+  //     'icon' => 'soundcloud.svg',
+  //     'link' => 'https://soundcloud.com/169cmpl',
+  //   ],
+  // ];
+  ?>
 
   <footer class='footer'>
-    <div class='footer__logo'>
-      <?= load_inline_svg('169cm_logo.svg'); ?>
-    </div>
-    <div class='footer__social-media'>
-      <?php Fragment_LinkIcons(['social-media'], $socialMedia); ?>
-    </div>
     <div class='footer__copyright'>
       &copy; <?= date('Y'); ?> <a href='<?= get_the_author_meta('user_url', 1); ?>' rel='noopener noreferrer'>Radosław Kalamon</a>
     </div>
