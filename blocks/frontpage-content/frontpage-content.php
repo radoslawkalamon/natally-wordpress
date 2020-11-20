c10607c3a034c40bb65b9af0060f6653994b2da7
Radoslaw Kalamon <16870210+radoslawkalamon@users.noreply.github.com>
2020-11-20 21:47:15 +0100

    [2.3.0] Frontpage content styling

--- blocks/frontpage-content/frontpage-content.php
+++ blocks/frontpage-content/frontpage-content.php
@@ -1,6 +1,8 @@
-<?php function Block_FrontpageContent() {
-  $frontpageContent = get_post(get_option( 'page_on_front'))->post_content;
-  $content = apply_filters('the_content', $frontpageContent);
+<?php function Block_FrontpageContent() { ?>
+  <?php $frontpageContent = get_post(get_option( 'page_on_front'))->post_content; ?>
+  <?php $content = apply_filters('the_content', $frontpageContent); ?>
 
-  Component_Content(['frontpage'], $content);
-}
+  <div class='frontpage-content'>
+    <?php Component_Content(['frontpage'], $content); ?>
+  </div>
+<?php }
