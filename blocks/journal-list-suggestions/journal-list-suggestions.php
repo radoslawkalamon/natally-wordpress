ca28c5b81f87f8c9046b38e18b8e5e1e33095711
Radoslaw Kalamon <16870210+radoslawkalamon@users.noreply.github.com>
2021-01-08 12:57:17 +0100

    [2.4.0] Journal Page const typo fix

--- blocks/journal-list-suggestions/journal-list-suggestions.php
+++ blocks/journal-list-suggestions/journal-list-suggestions.php
@@ -16,7 +16,7 @@
         'tag' => 'section',
         'title' => 'Zapiski',
         'buttonLabel' => 'Więcej Zapisków',
-        'buttonHref' => NATALLY_PAGE_JOURNALSS
+        'buttonHref' => NATALLY_PAGE_JOURNALS
       ],
       'Content_JournalListSuggestions',
       [],
