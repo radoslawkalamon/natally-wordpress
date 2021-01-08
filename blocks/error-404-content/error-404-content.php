2171654f721c8b4edc2d492cc9e408be401b8fa9
Radoslaw Kalamon <16870210+radoslawkalamon@users.noreply.github.com>
2021-01-08 16:59:52 +0100

    [2.4.0] Page 404 Content fix

--- blocks/error-404-content/error-404-content.php
+++ blocks/error-404-content/error-404-content.php
@@ -1,5 +1,5 @@
 <?php function Block_Error404Content() { ?>
-  <?php $content = apply_filters('the_content', get_the_content(NATALLY_PAGE_404)); ?>
+  <?php $content = apply_filters('the_content', get_the_content(null, false, NATALLY_PAGE_404)); ?>
 
   <div class='error-404-content'>
     <?php Component_Content(['poem'], $content); ?>
